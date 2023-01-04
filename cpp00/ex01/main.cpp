4424d7ab4d8b5fc28b08a27fdea2628060b2cf93
Badreldine Khatib <bkhatib@fu-r5-p1.s19.be>
2023-01-04 15:27:39 +0100

    some test to check if class Contact ok - need to add member function to print the member vars

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,7 +6,16 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 15:08:16 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/04 15:08:30 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/01/04 15:20:04 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Contact.hpp"
+
+int	main(void)
+{
+	Contact	contact1("b", "k", "bk", "5", "kb");
+	
+	std::cout << "first name:" << contact1.first_name << std::endl;
+	return(0);
+}
