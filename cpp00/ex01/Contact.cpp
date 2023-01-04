b255301b9b46ae1952e3fab3efefa666f46503e5
Badreldine Khatib <bkhatib@fu-r5-p1.s19.be>
2023-01-04 15:07:27 +0100

    some test on Contact constructor

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -6,7 +6,17 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 14:54:31 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/04 14:54:56 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/01/04 15:06:19 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Contact.hpp"
+
+Contact::Contact(std::string s1, std::string s2, std::string s3, std::string s4, std::string s5)
+{
+	first_name = s1;
+	last_name = s2;
+	nickname = s3;
+	phone_number = s4;
+	darkest_secret = s5;
+}
