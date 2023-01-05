c78aa4033e108b759f89d2f38f7c9890882dc167
baki <badrkhatib@hotmail.com>
2023-01-05 16:52:05 +0100

    Done with member fct set_values

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -6,20 +6,22 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 14:54:31 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/04 16:09:25 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/01/05 16:46:37 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 
-Contact::Contact (std::string s1, std::string s2, std::string s3, std::string s4, std::string s5)
+Contact::Contact() {};
+
+void Contact::set_values(std::string s1, std::string s2, std::string s3, std::string s4, std::string s5)
 {
 	first_name = s1;
 	last_name = s2;
 	nickname = s3;
 	phone_number = s4;
 	darkest_secret = s5;
-};
+}
 
 void Contact::print_info (void)
 {
