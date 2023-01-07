a7d5205fd35db1375f4a6d3a1b7d842451f0adf2
baki <badrkhatib@hotmail.com>
2023-01-07 19:25:35 +0100

    maj mise en forme

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 16:43:56 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/07 19:01:42 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/01/07 19:23:40 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,22 @@ PhoneBook::PhoneBook()
 {
 	numContact = 0;
 	done = 0;
-	std::cout << "Welcome in your phone book.\nYour phone book is actually empty but you can ADD a contact whenever you want :)." <<std::endl;
-	std::cout << "You can then SEARCH in your phone book or EXIT when you're done." <<std::endl;
-	std::cout << "You can choose now between those three command:" <<std::endl;
-	std::cout << "1 - ADD\n2 - SEARCH\n3 - EXIT" <<std::endl;
+	std::cout << "Welcome in your phone book.\nYou can choose between those three commands:" <<std::endl;
+	std::cout << "ADD - To add a contact\nSEARCH - To search for a contact\nEXIT - To exit your phone book" <<std::endl;
 }
 
 void	PhoneBook::add()
 {
 	std::string	fN, lN, nN, pN, dS;
-	std::cout << "Enter the first name:" <<std::endl;
+	std::cout << "Enter the first name: ";
 	std::cin >> fN;
-	std::cout << "Enter the last name:" <<std::endl;
+	std::cout << "Enter the last name: ";
 	std::cin >> lN;
-	std::cout << "Enter the nickname:" <<std::endl;
+	std::cout << "Enter the nickname: ";
 	std::cin >> nN;
-	std::cout << "Enter the phone number:" <<std::endl;
+	std::cout << "Enter the phone number: ";
 	std::cin >> pN;
-	std::cout << "Enter the darkest secret:" <<std::endl;
+	std::cout << "Enter the darkest secret: ";
 	std::cin >> dS;
 	if(numContact == 7)
 		cont[0].setContact(fN, lN, nN, pN, dS);
