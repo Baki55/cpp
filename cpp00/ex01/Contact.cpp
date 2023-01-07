237ab8c71996124f46bb86e936148ceb1e215442
baki <badrkhatib@hotmail.com>
2023-01-07 16:22:57 +0100

    maj norme -> UpperCamelCase format

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 14:54:31 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/05 16:46:37 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/01/07 16:21:24 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 Contact::Contact() {};
 
-void Contact::set_values(std::string s1, std::string s2, std::string s3, std::string s4, std::string s5)
+void Contact::setContact(std::string s1, std::string s2, std::string s3, std::string s4, std::string s5)
 {
-	first_name = s1;
-	last_name = s2;
+	firstName = s1;
+	lastName = s2;
 	nickname = s3;
-	phone_number = s4;
-	darkest_secret = s5;
+	phoneNumber = s4;
+	darkestSecret = s5;
 }
 
 void Contact::print_info (void)
 {
-	std::cout << "First name: " << first_name << std::endl;
-	std::cout << "Last name: " << last_name << std::endl;
+	std::cout << "First name: " << firstName << std::endl;
+	std::cout << "Last name: " << lastName << std::endl;
 	std::cout << "nickname: " << nickname << std::endl;
-	std::cout << "Phone number: " << phone_number << std::endl;
-	std::cout << "Darkest secret: " << darkest_secret << std::endl;
+	std::cout << "Phone number: " << phoneNumber << std::endl;
+	std::cout << "Darkest secret: " << darkestSecret << std::endl;
 };
