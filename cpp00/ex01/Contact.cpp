a1215457d0e567a8d80a816d9f99b4a3e66764ac
Badreldine Khatib <bkhatib@fu-r5-p1.s19.be>
2023-01-10 15:55:50 +0100

    maj member fct getContact to display correctly the variables

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 14:54:31 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/07 17:18:07 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/01/10 15:40:43 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,17 @@ void Contact::setContact(std::string s1, std::string s2, std::string s3, std::st
 	std::cout << "The contact is successfully added to your Phonebook." << std::endl;
 }
 
+std::string Contact::getStr(std::string str) const
+{
+	if(str.length() > 10)
+		return str.substr(0, 9) + ".";
+	return str;
+}
+
 void Contact::getContact(void)
 {
-	std::cout << "First name: " << firstName << std::endl;
-	std::cout << "Last name: " << lastName << std::endl;
-	std::cout << "Nickname: " << nickname << std::endl;
-	std::cout << "Phone number: " << phoneNumber << std::endl;
-	std::cout << "Darkest secret: " << darkestSecret << std::endl;
+	std::cout << "|" << std::setw(10) << this->getStr(this->firstName) << std::flush;
+	std::cout << "|" << std::setw(10) << this->getStr(this->lastName) << std::flush;
+	std::cout << "|" << std::setw(10) << this->getStr(this->nickname) << std::flush;
+	std::cout << "|" << std::endl;
 };
