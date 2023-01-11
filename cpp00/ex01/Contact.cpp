0c8285498876ad5d5f99f8c8b5d60e9533e9432b
baki <badrkhatib@hotmail.com>
2023-01-11 15:47:07 +0100

    done with the new member function getinfo() to retrieve all the info of a contact

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 14:54:31 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/10 15:40:43 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/01/11 15:44:28 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 Contact::Contact() {};
 
-void Contact::setContact(std::string s1, std::string s2, std::string s3, std::string s4, std::string s5)
+void	Contact::setContact(std::string s1, std::string s2, std::string s3, std::string s4, std::string s5)
 {
 	firstName = s1;
 	lastName = s2;
@@ -24,17 +24,27 @@ void Contact::setContact(std::string s1, std::string s2, std::string s3, std::st
 	std::cout << "The contact is successfully added to your Phonebook." << std::endl;
 }
 
-std::string Contact::getStr(std::string str) const
+std::string	Contact::getStr(std::string str) const
 {
 	if(str.length() > 10)
 		return str.substr(0, 9) + ".";
 	return str;
 }
 
-void Contact::getContact(void)
+void	Contact::getContact(void)
 {
 	std::cout << "|" << std::setw(10) << this->getStr(this->firstName) << std::flush;
 	std::cout << "|" << std::setw(10) << this->getStr(this->lastName) << std::flush;
 	std::cout << "|" << std::setw(10) << this->getStr(this->nickname) << std::flush;
 	std::cout << "|" << std::endl;
-};
+}
+
+void	Contact::getInfo(void)
+{
+	std::cout << "|  " << this->firstName << std::flush;
+	std::cout << "  |  " << this->lastName << std::flush;
+	std::cout << "  |  " << this->nickname << std::flush;
+	std::cout << "  |  " << this->phoneNumber << std::flush;
+	std::cout << "  |  " << this->darkestSecret << std::flush;
+	std::cout << "  |" << std::endl; 
+}
