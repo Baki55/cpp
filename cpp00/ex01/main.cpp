5b967c637bb07fef02938659eeb256e9161cc2e3
baki <badrkhatib@hotmail.com>
2023-01-12 15:39:50 +0100

    added a message on console for the command

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 15:08:16 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/11 17:02:31 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/01/12 15:35:38 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int	main(void)
 	
 	while (myPhoneBook.done == 0)
 	{
+		std::cout << "Choose a command: ";
 		std::cin >> cmd;
 		if(cmd == "ADD")
 			myPhoneBook.add();
