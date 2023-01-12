e8063d3d7564e65cb0fea13943f1bccb76107a72
baki <badrkhatib@hotmail.com>
2023-01-12 16:13:35 +0100

    Added the class Zombie and its attributs

---
+++ cpp01/ex00/Zombie.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 15:50:39 by bkhatib           #+#    #+#             */
+/*   Updated: 2023/01/12 16:12:27 by bkhatib          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class	Zombie
+{
+	std::string	name;
+public:
+	Zombie();
+	~Zombie();
+	void	annouce(void) const;
+	Zombie	*newZombie(std::string name);
+	void	randomChump(std::string name);
+};
