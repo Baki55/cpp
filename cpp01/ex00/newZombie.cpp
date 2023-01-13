059e2f130d34ef240a8819ae265faadfed3311b1
baki <badrkhatib@hotmail.com>
2023-01-13 16:19:46 +0100

    done with newZombie function

--- cpp01/ex00/newZombie.cpp
+++ cpp01/ex00/newZombie.cpp
@@ -6,8 +6,16 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 15:56:17 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/13 15:56:28 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/01/13 16:17:07 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+Zombie	*newZombie(std::string str)
+{
+	Zombie	*newZombie = new Zombie;
+	
+	newZombie->setName(str);
+	return(newZombie);
+}
