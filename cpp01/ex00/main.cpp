b1169307616ed49a8415d65bacc827036659a9b9
baki <badrkhatib@hotmail.com>
2023-01-13 16:21:20 +0100

    some tests

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 16:26:26 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/12 16:41:04 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/01/13 15:36:51 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 
 int	main(void)
 {
-	Zombie	myZombie;
-	
-	myZombie.setName("Baki");
-	myZombie.annouce();
+	randomChump("Baki");
 	return (0);
 }
