e08355dec2e110cecc976bf70090f1bea2a35456
baki <badrkhatib@hotmail.com>
2023-01-14 13:42:53 +0100

    added delete on every Zombie

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 12:30:53 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/14 13:17:37 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/01/14 13:32:15 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ int	main(void)
 	nZombie = zombieHorde(n, "Baki");
 	for(int i = 0; i < n; i++)
 		nZombie[i].annouce();
+	delete [] nZombie;
 	return(1);
 }
