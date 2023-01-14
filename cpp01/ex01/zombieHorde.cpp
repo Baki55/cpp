99bd63936ad8ae1d26278f08596c70d9e49dfeb6
baki <badrkhatib@hotmail.com>
2023-01-14 13:19:02 +0100

    done with the zombieHorde(int, string) fucntion

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 12:26:35 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/14 12:28:18 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/01/14 12:52:57 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 Zombie	*zombieHorde(int N, std::string name)
 {
+	Zombie	*nZombie = new Zombie[N];
 	
+	for(int i = 0; i < N; i++)
+		nZombie[i].setName(name);
+	return(nZombie);
 }
