85bd7996c77cc51940339222117d0ff88ddae81a
Badreldine Khatib <bkhatib@fu-r8-p2.s19.be>
2023-01-16 17:25:38 +0100

    removed the constructor message

--- cpp01/ex03/HumanA.cpp
+++ cpp01/ex03/HumanA.cpp
@@ -6,16 +6,13 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 17:30:01 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/16 16:54:24 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/01/16 17:15:22 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 
-HumanA::HumanA(std::string str, Weapon &myW) : name(str), myWeapon(myW)
-{
-	std::cout << "My name is " << this->name <<std::endl;
-}
+HumanA::HumanA(std::string str, Weapon &myW) : name(str), myWeapon(myW) {}
 
 HumanA::~HumanA(){}
 
