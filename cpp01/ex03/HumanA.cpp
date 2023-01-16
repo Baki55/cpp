657df38f0900e4cf4ea9a4dadb5079c544753c46
Badreldine Khatib <bkhatib@fu-r8-p2.s19.be>
2023-01-16 16:56:20 +0100

    maj on Weapon variable in HumanA class -> myWeapon is a reference now

--- cpp01/ex03/HumanA.cpp
+++ cpp01/ex03/HumanA.cpp
@@ -6,26 +6,20 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 17:30:01 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/15 18:03:50 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/01/16 16:54:24 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 
-HumanA::HumanA(std::string str, Weapon myW)
+HumanA::HumanA(std::string str, Weapon &myW) : name(str), myWeapon(myW)
 {
-	std::cout << "Constructor called." << std::endl;
-	this->name = str;
-	this->myWeapon = myW;
 	std::cout << "My name is " << this->name <<std::endl;
 }
 
-HumanA::~HumanA()
-{
-	std::cout << "Destructor called" << std::endl;
-}
+HumanA::~HumanA(){}
 
 void	HumanA::attack()
 {
-	std::cout << this->name << " attacks with their " << std::endl;
+	std::cout << this->name << " attacks with their " << this->myWeapon.getType() << std::endl;
 }
--- cpp01/ex03/HumanA.hpp
+++ cpp01/ex03/HumanA.hpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 17:21:52 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/15 17:54:48 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/01/16 16:51:53 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@
 class	HumanA
 {
 	std::string	name;
-	Weapon	myWeapon;
+	Weapon	&myWeapon;
 public:
-	HumanA(std::string, Weapon);
+	HumanA(std::string str, Weapon &myW);
 	~HumanA();
 	void	attack();
 };
