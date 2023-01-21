56bbe511d53bacd15915d98dbed5a66450e43996
baki <badrkhatib@hotmail.com>
2023-01-21 17:00:14 +0100

    added ';'

--- cpp01/ex04/Sed.hpp
+++ cpp01/ex04/Sed.hpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 16:10:19 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/21 16:20:00 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/01/21 16:33:27 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ class	Sed
 public:
 	Sed(std::string);
 	void	replace(std::string, std::string);
-}
+};
 
 #endif
