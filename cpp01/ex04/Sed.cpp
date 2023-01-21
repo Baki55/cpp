f309ecf0c87f94b480b00ad6276223217875d6b5
baki <badrkhatib@hotmail.com>
2023-01-21 17:00:51 +0100

    implemented the replace member fct

--- cpp01/ex04/Sed.cpp
+++ cpp01/ex04/Sed.cpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 16:16:04 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/21 16:28:37 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/01/21 16:58:56 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,27 @@ void	Sed::replace(std::string toFind, std::string toReplaceW)
 	std::ifstream	ifs(this->inFile);
 	if(ifs.is_open())
 	{
-		std::cout << "The file is succefully opened." << std::endl;
-		ifs.close();
+		std::string	content;
+		if(std::getline(ifs, content, '\0'))
+		{
+			std::ofstream	ofs(this->outFile);
+			size_t	found = content.find(toFind);
+			while(found != std::string::npos)
+			{
+				content.erase(found, toFind.length());
+				content.insert(found, toReplaceW);
+				found = content.find(toFind);
+			}
+			ofs << content;
+			ofs.close();
+			ifs.close();
+		}
+		else
+			std::cerr << "Unable to read the file" << std::endl;
 	}
 	else
 	{
-		std::cerr << "Unable to open " << this->infile << std::endl;
+		std::cerr << "Unable to open " << this->inFile << std::endl;
 		exit(EXIT_FAILURE);
+	}
 }
