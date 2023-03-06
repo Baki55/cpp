6945814122e17c2ce2ecea25d036287531340e60
baki <badrkhatib@hotmail.com>
2023-03-06 19:09:09 +0100

    some tests

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 14:47:13 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/03/06 14:50:30 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/03/06 19:04:13 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,14 @@
 int	main()
 {
 	Harl	harl;
+	std::string	input;
 
+	do
+	{
+		std::cout << "Choose a level: ";
+		std::cin >> input;
+		harl.complain(input);
+	} while(input.compare("exit"));
+	
 	return (0);
 }
