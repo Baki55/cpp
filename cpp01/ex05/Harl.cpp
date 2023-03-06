66041970fad73693280b8eee7ec69c9c48b54791
baki <badrkhatib@hotmail.com>
2023-03-06 19:08:43 +0100

    Maj on msg that are displayed by Harl

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 14:44:18 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/03/06 18:48:19 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/03/06 19:07:14 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,25 @@ Harl::~Harl()
 
 void	Harl::debug(void)
 {
-	std::cout << "Debug" <<std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-
+ketchup burger. I really do !" <<std::endl;
 }
 
 void	Harl::info(void)
 {
-	std::cout << "Info" <<std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put
+enough bacon in my burger ! If you did, I wouldn’t be asking for more !" <<std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << "Warning" <<std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming
+for years whereas you started working here since last month." <<std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cout << "Error" <<std::endl;
+	std::cout << "This is unacceptable ! I want to speak to the manager now." <<std::endl;
 }
 
 void	Harl::complain(std::string level)
