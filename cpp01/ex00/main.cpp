916d1745ea9c0884181ca325cd40abf2895c1bd5
Badreldine Khatib <bkhatib@shi-r8-p1.s19.be>
2023-03-06 12:02:05 +0100

    some tests

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 16:26:26 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/01/14 11:57:54 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/03/06 12:00:58 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,15 @@
 int	main(void)
 {
 	Zombie	zombie[2];
-	
+	Zombie	*z;
+
 	zombie[0].setName("Baki");
-	zombie[1].setName("Messi");
 	zombie[0].annouce();
+	zombie[1].setName("Messi");
 	zombie[1].annouce();
+	z = newZombie("Bib");
+	z->annouce();
+	delete z;
+	randomChump("Bob");
 	return (0);
 }
