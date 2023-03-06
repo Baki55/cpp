2ab7fe679e74a041317847edc1388bb2a73fac52
baki <badrkhatib@hotmail.com>
2023-03-06 18:59:03 +0100

    implemented Harl::complain()

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 14:44:18 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/03/06 14:53:36 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/03/06 18:48:19 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,33 @@ Harl::~Harl()
 	std::cout << "Destructor called" <<std::endl;
 }
 
-Harl::debug(void)
+void	Harl::debug(void)
 {
 	std::cout << "Debug" <<std::endl;
 }
 
-Harl::info(void)
+void	Harl::info(void)
 {
 	std::cout << "Info" <<std::endl;
 }
 
-Harl::warning(void)
+void	Harl::warning(void)
 {
 	std::cout << "Warning" <<std::endl;
 }
 
-Harl::error(void)
+void	Harl::error(void)
 {
 	std::cout << "Error" <<std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	t_func	f[] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string levels[] = {"debug", "info", "warning", "error"};
+	int i = 0;
+	while (i < 4 && levels[i].compare(level))
+		i++;
+	if (i < 4)
+		(this->*f[i])();
 }
