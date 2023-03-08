20dd6456a563a4da4de480668a49c72e716faa9c
Badreldine Khatib <bkhatib@shi-r8-p5.s19.be>
2023-03-08 15:11:30 +0100

    maj on the copy assignment operator overload -> return now reference

--- cpp02/ex00/can.hpp
+++ cpp02/ex00/can.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   can.hpp                                            :+:      :+:    :+:   */
+/*   Can.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 10:15:33 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/03/07 11:10:10 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/03/08 14:59:34 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ class	FixedPoint
 	int static const	fractionalBits = 8;
 public:
 	FixedPoint();
-	FixedPoint(const Fixed &rhs);
-	Fixed& operator=(const Fixed &rhs);
+	FixedPoint(const FixedPoint &rhs);
 	~FixedPoint();
+	FixedPoint& operator=(const FixedPoint &rhs);
 	int	getRawBits(void) const;
 	void	setRawBits(int const raw);
 };
