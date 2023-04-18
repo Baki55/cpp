ba9501f17a026ae2b8dd2f0d69b0e95f8be91514
baki <badrkhatib@hotmail.com>
2023-04-18 18:24:18 +0200

    added WrongCat.hpp file

---
+++ cpp04/ex00/WrongCat.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongCat.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/18 18:22:48 by bkhatib           #+#    #+#             */
+/*   Updated: 2023/04/18 18:23:44 by bkhatib          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WRONGCAT_HPP
+#define WRONGCAT_HPP
+
+#include "WrongAnimal.hpp"
+
+class WrongCat : public WrongAnimal
+{
+private:
+
+public:
+	WrongCat( void );
+	~WrongCat( void );
+
+	void makeSound() const;
+};
+
+#endif
