5c3eaeaea10dbe53e7cf295559c38632a32b5cf9
baki <badrkhatib@hotmail.com>
2023-04-18 16:40:14 +0200

    some tests

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,20 +6,24 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 19:28:37 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/04/18 13:33:34 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/04/18 16:25:19 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
 
-int	main(void)
+int main()
 {
-	Animal a("Baki");
-	Dog d;
-	
-	d.makeSound();
-	std::cout << d.getType() << std::endl;
-	std::cout << a.getType() << std::endl;
-	
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	std::cout << j->getType() << " " << std::endl; std::cout << i->getType() << " " << std::endl; i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+	delete meta;
+	delete j;
+	delete i;
 	return(EXIT_SUCCESS);
 }
