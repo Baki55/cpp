503d6c541af26d994a0b5920d2b7d9c98f90dbe2
baki <badrkhatib@hotmail.com>
2023-04-18 18:29:31 +0200

    some tests

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,24 +6,40 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 19:28:37 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/04/18 16:25:19 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/04/18 18:27:46 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
 int main()
 {
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
-	std::cout << j->getType() << " " << std::endl; std::cout << i->getType() << " " << std::endl; i->makeSound(); //will output the cat sound!
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
 	j->makeSound();
 	meta->makeSound();
 	delete meta;
 	delete j;
 	delete i;
+	
+	const WrongAnimal *wrong = new WrongAnimal();
+    const WrongAnimal *wrongCat = new WrongCat();
+
+    std::cout << "Wrong Type: " << wrong->getType() << " " << std::endl;
+    std::cout << "WrongCat Type: " << wrongCat->getType() << " " << std::endl;
+    wrong->makeSound();
+    wrongCat->makeSound();
+
+    delete  wrong;
+    delete  wrongCat;
+	
 	return(EXIT_SUCCESS);
 }
