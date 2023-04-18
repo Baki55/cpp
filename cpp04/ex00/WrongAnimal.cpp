c754abd2ab39e16d390123657f9d223a6a2d645a
baki <badrkhatib@hotmail.com>
2023-04-18 18:34:29 +0200

    maj on default constructor msg

--- cpp04/ex00/WrongAnimal.cpp
+++ cpp04/ex00/WrongAnimal.cpp
@@ -6,20 +6,20 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 18:18:05 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/04/18 18:21:47 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/04/18 18:33:36 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "WrongAnimal.hpp"
 
-WrongAnimal::WrongAnimal() : type("WrongAnimel")
+WrongAnimal::WrongAnimal() : type("WrongAnimal")
 {
-	std::cout << this->type << "Default constructor (WrongAnimal) constructor called" << std::endl;
+	std::cout << this->type << " Default constructor (WrongAnimal) constructor called" << std::endl;
 }
 
 WrongAnimal::WrongAnimal(std::string type) : type(type)
 {
-	std::cout << "WrongAnimal " << this->type << "Second constructor (WrongAnimal) called" << std::endl;
+	std::cout << "WrongAnimal " << this->type << " Second constructor (WrongAnimal) called" << std::endl;
 }
 
 WrongAnimal::~WrongAnimal()
