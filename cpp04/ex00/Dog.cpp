5ba266cd616217c2b68ad12d25d32ebb2752c1c0
baki <badrkhatib@hotmail.com>
2023-04-18 13:36:45 +0200

    implemented all the member fct of Dog class

---
+++ cpp04/ex00/Dog.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 19:57:13 by bkhatib           #+#    #+#             */
+/*   Updated: 2023/04/18 13:28:52 by bkhatib          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+
+Dog::Dog()
+{
+	std::cout << "Default Dog constructor called." << std::endl;
+	this->type = "Dog";
+}
+
+Dog::Dog(Dog &rhs)
+{
+	std::cout << "Copy constructor called." << std::endl;
+	*this = rhs;
+}
+
+Dog::~Dog()
+{
+	std::cout << "Default dog destructor called." << std::endl;
+}
+
+Dog &Dog::operator=(const Dog &rhs)
+{
+	std::cout << "Copy assignement operator called." << std::endl;
+	this->type = rhs.type;
+	return(*this);
+}
+
+void	Dog::makeSound()
+{
+	std::cout << "Waf Waf :)" << std::endl;
+}
