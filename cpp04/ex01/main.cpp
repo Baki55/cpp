e6ad4f2458556566f5a191de5ec3acde41f8a147
baki <badrkhatib@hotmail.com>
2023-04-19 16:05:07 +0200

    some tests

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,45 +6,32 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 19:28:37 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/04/18 18:32:24 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/04/19 15:23:28 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
 #include "Cat.hpp"
-#include "Dog.hpp"
-#include "WrongAnimal.hpp"
+
 #include "WrongCat.hpp"
 
-int main()
+int main( void )
 {
-	std::cout << "@@@@@@@@@@ Animal @@@@@@@@@@" << std::endl;
-	
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-	delete meta;
-	delete j;
-	delete i;
-	
-	std::cout << std::endl << "@@@@@@@@@@ WrongAnimal @@@@@@@@@@" << std::endl;
-	
-	const WrongAnimal *wrong = new WrongAnimal();
-    const WrongAnimal *wrongCat = new WrongCat();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
 
-	std::cout << "Wrong Type: " << wrong->getType() << " " << std::endl;
-	std::cout << "WrongCat Type: " << wrongCat->getType() << " " << std::endl;
-	wrong->makeSound();
-	wrongCat->makeSound();
+    delete j;//should not create a leak
+    delete i;
 
-	delete  wrong;
-	delete  wrongCat;
-	
-	return(EXIT_SUCCESS);
+    Dog basic;
+    {
+        Dog tmp = basic;
+    }
+
+    const Animal* animals[4] = { new Dog(), new Dog(), new Cat(), new Cat() };
+    for ( int i = 0; i < 4; i++ ) {
+        delete animals[i];
+    }
+
+    return 0;
 }
