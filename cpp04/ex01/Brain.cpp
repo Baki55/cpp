67069a58a1e15b420df051bf066ee20112d90bf3
baki <badrkhatib@hotmail.com>
2023-04-19 16:04:47 +0200

    maj on var name

--- cpp04/ex01/Brain.cpp
+++ cpp04/ex01/Brain.cpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 15:18:07 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/04/19 15:32:39 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/04/19 16:03:49 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,28 @@
 
 Brain::Brain()
 {
-    std::cout << "Brain created." << std::endl;
+	std::cout << "Brain created." << std::endl;
 }
 
 Brain::~Brain()
 {
-    std::cout << "Brain destroyed." << std::endl;
+	std::cout << "Brain destroyed." << std::endl;
 }
 
 Brain::Brain(const Brain &rhs)
 {
-    *this = rhs;
+	*this = rhs;
 }
 
 Brain& Brain::operator=(const Brain &rhs)
 {
-    std::cout << "<Brain.cpp:32> Brain copy called." << std::endl;
-    if (this != &rhs)
-    {
-        for (int i = 0; i < 100; i++)
-        {
-            this->ideas[i] = rhs.ideas[i];
-        }
-    }
-    return *this;
+	std::cout << "<Brain.cpp:32> Brain copy called." << std::endl;
+	if (this != &rhs)
+	{
+		for (int i = 0; i < 100; i++)
+		{
+			this->ideas[i] = rhs.ideas[i];
+		}
+	}
+	return *this;
 }
