0998fdf83c772440df392f3dc58ee4f3c0958f82
Badreldine Khatib <bkhatib@fu-r9-p8.s19.be>
2023-04-24 12:13:12 +0200

    maj on constructor/destructor and msgs displayed by mem fcts

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -6,43 +6,34 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 14:44:18 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/03/06 19:07:14 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/04/24 12:09:56 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-Harl::Harl()
-{
-	std::cout << "Constructor called" <<std::endl;
-}
+Harl::Harl() {}
 
-Harl::~Harl()
-{
-	std::cout << "Destructor called" <<std::endl;
-}
+Harl::~Harl() {}
 
 void	Harl::debug(void)
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-
-ketchup burger. I really do !" <<std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do !" << std::endl;
 }
 
 void	Harl::info(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put
-enough bacon in my burger ! If you did, I wouldn’t be asking for more !" <<std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger ! If you did, I wouldn’t be asking for more !" << std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming
-for years whereas you started working here since last month." <<std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cout << "This is unacceptable ! I want to speak to the manager now." <<std::endl;
+	std::cout << "This is unacceptable ! I want to speak to the manager now." << std::endl;
 }
 
 void	Harl::complain(std::string level)
