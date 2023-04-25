bfb0f0b01b1d685691bf6bc867785bd7370fbdbd
baki <badrkhatib@hotmail.com>
2023-04-25 17:31:54 +0200

    maj on constructor + implemented mem fct attack(&target)

--- cpp03/ex01/ScavTrap.cpp
+++ cpp03/ex01/ScavTrap.cpp
@@ -6,25 +6,34 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 19:50:31 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/04/17 18:35:03 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/04/25 17:26:01 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 
-ScavTrap::ScavTrap()
-{
-	std::cout << "Default ScavTrap constructor called." << std::endl;
-}
-
 ScavTrap::ScavTrap(std::string str): ClapTrap(str)
 {
-	std::cout << "Second ScavTrap constructor called." << std::endl;
+	std::cout << "ScavTrap constructor called." << std::endl;
+	this->hitPoints = 100;
+	this->energyPoints = 50;
+	this->attackDamage = 20;
 }
 
 ScavTrap::~ScavTrap()
 {
-	std::cout << "Default ScavTrap destructor called." << std::endl;
+	std::cout << "ScavTrap destructor called." << std::endl;
+}
+
+void	ScavTrap::attack(const std::string &target)
+{
+	if(this->energyPoints <= 0)
+		std::cout << "ScravTrap " << this->name << " is out of ernery." << std::endl;
+	else
+	{
+		std::cout << "ScavTrap " << this->name << " attacks " << target << " and causing " << this->attackDamage << " damage." << std::endl;
+		this->energyPoints -= 1;
+	}
 }
 
 void	ScavTrap::guardGate()
