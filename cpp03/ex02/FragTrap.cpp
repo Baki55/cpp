5ddc5b7fb719192fc8ad24c7f7e6b18cbcbe330c
baki <badrkhatib@hotmail.com>
2023-04-25 17:50:25 +0200

    maj on cout + constructor (init member)

--- cpp03/ex02/FragTrap.cpp
+++ cpp03/ex02/FragTrap.cpp
@@ -6,25 +6,23 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 20:29:25 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/04/17 18:48:22 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/04/25 17:47:52 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
 
-FragTrap::FragTrap()
-{
-	std::cout << "Default FragTrap constructor called." << std::endl;
-}
-
 FragTrap::FragTrap(std::string str): ClapTrap(str)
 {
-	std::cout << "Second FragTrap constructor called" << std::endl;
+	this->hitPoints = 100;
+	this->energyPoints = 100;
+	this->attackDamage = 30;
+	std::cout << "FragTrap constructor called" << std::endl;
 }
 
 FragTrap::~FragTrap()
 {
-	std::cout << "Default destructor FragTrap called." << std::endl;
+	std::cout << "FragTrap destructor called." << std::endl;
 }
 
 void	FragTrap::highFivesGuys()
