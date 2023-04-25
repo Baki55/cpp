8d0bb77cbd560d5893d752cd774587c9ca099f73
baki <badrkhatib@hotmail.com>
2023-04-25 17:49:38 +0200

    del one constructor

--- cpp03/ex02/FragTrap.hpp
+++ cpp03/ex02/FragTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 20:15:16 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/03/15 20:29:02 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/04/25 17:42:32 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 class FragTrap : public ClapTrap
 {
 public:
-	FragTrap();
 	FragTrap(std::string);
 	~FragTrap();
 	void	highFivesGuys(void);
