1b7ddb0824ae3b57a70242e0d3e453e2ecbb90c7
baki <badrkhatib@hotmail.com>
2023-04-27 15:38:21 +0200

    maj on msg Cat constructor

--- cpp04/ex00/Cat.cpp
+++ cpp04/ex00/Cat.cpp
@@ -6,32 +6,31 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 13:46:42 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/04/18 16:13:19 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/04/27 15:30:35 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 
-Cat::Cat()
+Cat::Cat() : Animal("Cat")
 {
-	std::cout << "Default Cat constructor called." << std::endl;
-	this->type = "Cat";
+	std::cout << "Cat default constructor called." << std::endl;
 }
 
 Cat::Cat(Cat &rhs)
 {
-	std::cout << "Copy constructor called." << std::endl;
+	std::cout << "Cat copy constructor called." << std::endl;
 	*this = rhs;
 }
 
 Cat::~Cat()
 {
-	std::cout << "Default Cat destructor called." << std::endl;
+	std::cout << "Cat destructor called." << std::endl;
 }
 
 Cat &Cat::operator=(const Cat &rhs)
 {
-	std::cout << "Copy assignement operator called." << std::endl;
+	std::cout << "Cat copy assignement operator called." << std::endl;
 	this->type = rhs.type;
 	return(*this);
 }
