aafc78e8d00e44aa885153be58596b63eedf8e7c
baki <badrkhatib@hotmail.com>
2023-04-27 15:27:44 +0200

    maj displayed msg

--- cpp04/ex00/Animal.cpp
+++ cpp04/ex00/Animal.cpp
@@ -6,7 +6,7 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 19:13:11 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/04/18 18:11:44 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/04/27 15:25:12 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,28 @@
 
 Animal::Animal()
 {
-	std::cout << "Default constructor called." << std::endl;
+	std::cout << "Animal default constructor called." << std::endl;
 }
 
 Animal::Animal(std::string type) : type(type)
 {
-	std::cout << "Second constructor called." << std::endl;
+	std::cout << "Animal second constructor called." << std::endl;
 }
 
 Animal::Animal(Animal &rhs)
 {
-	std::cout << "Copy constructor called." << std::endl;
+	std::cout << "Animal copy constructor called." << std::endl;
 	*this = rhs;
 }
 
 Animal::~Animal()
 {
-	std::cout << "Default destructor called." << std::endl;
+	std::cout << "Animal destructor called." << std::endl;
 }
 
 Animal &Animal::operator=(const Animal &rhs)
 {
-	std::cout << "Copy assignement operator called." << std::endl;
+	std::cout << "Animal copy assignement operator called." << std::endl;
 	this->type = rhs.type;
 	return(*this);
 }
