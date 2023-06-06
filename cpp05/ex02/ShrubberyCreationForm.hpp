6477f7922bc2db2627bdf73ad38f08bc000b862b
baki <badrkhatib@hotmail.com>
2023-06-06 15:08:41 +0200

    added ShrubberyCreationForm class

---
+++ cpp05/ex02/ShrubberyCreationForm.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ShrubberyCreationForm.hpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/06 15:05:15 by bkhatib           #+#    #+#             */
+/*   Updated: 2023/06/06 15:08:10 by bkhatib          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHRUBBERYCREATIONFORM_HPP
+#define SHRUBBERYCREATIONFORM_HPP
+
+#include "Form.hpp"
+#include <fstream>
+
+class ShrubberyCreationForm : public Form {
+
+private:
+	const std::string	target;
+	ShrubberyCreationForm();
+public:
+	ShrubberyCreationForm(const std::string &target);
+	ShrubberyCreationForm(const ShrubberyCreationForm src);
+	~ShrubberyCreationForm();
+
+	ShrubberyCreationForm &operator=(ShrubberyCreationForm &rhs);
+
+	void execute(const Bureaucrat &executor) const;
+
+};
+
+#endif
