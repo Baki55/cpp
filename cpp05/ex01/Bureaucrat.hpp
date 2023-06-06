c006e67a2d3bea5824ff6f3bf700e6172f3c5e20
baki <badrkhatib@hotmail.com>
2023-06-06 16:38:50 +0200

    finally done with ex01

---
+++ cpp05/ex01/Bureaucrat.hpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/06 16:36:12 by bkhatib           #+#    #+#             */
+/*   Updated: 2023/06/06 16:36:21 by bkhatib          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include "Form.hpp"
+
+class Form;
+
+class Bureaucrat
+{
+private:
+    const std::string   _name;
+    int                 _grade;
+
+    Bureaucrat();
+
+public:
+    Bureaucrat( const std::string& name, int grade );
+    Bureaucrat( const Bureaucrat& src );
+    ~Bureaucrat();
+
+    Bureaucrat& operator=( const Bureaucrat& rhs );
+
+    std::string getName() const;
+    int         getGrade() const;
+
+    void        incrementGrade();
+    void        decrementGrade();
+
+    void        signForm( Form& form );
+
+    /* ---------------- Exception Classes ---------------- */
+    class GradeTooHighException : public std::exception {
+        public:
+            virtual const char* what() const throw() { return "Grade too high"; }
+    };
+    class GradeTooLowException : public std::exception {
+        public:
+            virtual const char* what() const throw() { return "Grade too low"; }
+    };
+
+};
+
+std::ostream&   operator<<( std::ostream& o, const Bureaucrat& rhs );
+
+#endif
---
+++ cpp05/ex01/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/06 16:36:46 by bkhatib           #+#    #+#             */
+/*   Updated: 2023/06/06 16:37:45 by bkhatib          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+int main( void )
+{
+
+    try {
+        Bureaucrat bureaucrat("BK",11);
+        Form form("formName", 10);
+
+        bureaucrat.signForm(form);
+
+        std::cout << form << std::endl;
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+    return EXIT_SUCCESS;
+}
