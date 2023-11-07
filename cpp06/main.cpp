de2ba775acfe181d361fc0eff371f28b75e87778
baki <badrkhatib@hotmail.com>
2023-11-07 13:00:49 +0100

    some tests

--- cpp06/main.cpp
+++ cpp06/main.cpp
@@ -6,43 +6,29 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 15:11:32 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/10/31 16:59:28 by bkhatib          ###   ########.fr       */
+/*   Updated: 2023/10/31 21:57:57 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <sstream>
-
-bool	isDouble(std::string _str)
-{
-	if(_str.find( '.' ) == std::string::npos || _str.find( '.' ) == 0 || _str.find( '.' ) == _str.length() - 1)
-		return(0);
-	int	found = 0;
-	int	i = 0;
-	while(_str[i] == '-' || _str[i] == '+')
-		i++;
-	while(i < (int)_str.length())
-	{
-		if(_str[i] == '.')
-			found++;
-		if((!std::isdigit(_str[i]) && _str[i] != '.' ) || found > 1)
-			return(0);
-		i++;
-	}
-	return(1);
-}
+#include "Convert.hpp"
 
 int main(int argc, char **argv)
 {
 	if(argc != 2)
-		std::cout << "Bad arguments" << std::endl;
-	else
 	{
-		std::string	str = argv[1];
-		if(isDouble(str))
-			std::cout << "True" << std::endl;
-		else
-			std::cout << "False" << std::endl;
+		std::cout << "Usage: ./convert numberToConvert" << std::endl;
+		return(EXIT_FAILURE);
 	}
-	return(1);
+	try
+	{
+		ScalarConverter	sC;
+		std::string str = argv[1];
+		sC.convert(str);
+		std::cout << sC;
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
+	return(EXIT_SUCCESS);
 }
